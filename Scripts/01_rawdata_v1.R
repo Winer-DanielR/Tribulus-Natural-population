c1ce032c4bfe26d813faf2cef94aa73fdb611853
Winer Daniel Reyes Corral <winerdreyes@gmail.com>
2020-06-11 13:05:39 -0400

    Clean data, filtered NAs for PCAs on morphology for years 2017 - 2019

--- Scripts/01_rawdata_v1.R
+++ Scripts/01_rawdata_v1.R
@@ -5,8 +5,9 @@
 ### Record of various populations of Tribulus in four islands in Galapagos from 2015 to 2019
 ### Record of predation of birds and insects (in 2019 only)
 ### By Daniel Reyes Corral
-### Raw data
 
+### Raw data
+#Tribulus1 has replaces all spine distances of 0 into NAs
 tribulus <- read.csv("~/R/Tribulus/Tribulus Natural populations/Tribulus-Natural-population/Data/Raw/All time Tribulus1.csv")
 head(natpop)
 summary(natpop)
@@ -14,9 +15,65 @@ summary(natpop)
 #Extracting data from 2017 - 2019
 year <- filter(tribulus, year >= 2017)
 View(year)
+clean_l <- filter(year, !is.na(length))
+clean_d <- filter(clean_l, !is.na(depth))
+clean_s <- filter(clean_d, !is.na(longest_spine))
 
 
+################spine tip distance measurements were ommited in 2019. This is a PCA of all variables BUT spine distance from 2017 - 2019
+tribulus <- clean_s
+Pr <- prcomp(tribulus[,8:11], scale. = TRUE)
+Pr
+summary(Pr)
+plot(Pr, type = "l")
+biplot(Pr, scale = 0)
 
-#Select mophological variables: length, width, depth, spine distance, spine length
-morph <- prcomp(natpop[,])
+#Extracting PC scores
+str(Pr)
+Pr$x
+#Adding PC scores to dataset
+tribulusPC <- cbind(tribulus, Pr$x[,1:3])
+head(tribulusPC)
+#plot with ggplot
+ggplot(tribulusPC, aes(PC1, PC2, col = island)) + stat_ellipse(geom = "polygon", col = "black", alpha = 0.5) + geom_point(shape = 21, col = "black")
+
+###############Including spine distance BUT ommiting 2019 dataaset
+clean_sd <- filter(clean_s, !is.na(spine_tip_distance))
+PrSd <- prcomp(clean_sd[,8:12], scale. = TRUE)
+PrSd
+summary(PrSd)
+plot(PrSd, type = "l")
+biplot(PrSd, scale = 0)
+
+#Extracting PC scores
+str(PrSd)
+PrSd$x
+#Adding PC scores to dataset
+tribulusSD <- cbind(clean_sd, PrSd$x[,1:2])
+head(tribulusSD)
+#plot with ggplot
+ggplot(tribulusSD, aes(PC1, PC2, col = island)) + stat_ellipse(geom = "polygon", col = "black", alpha = 0.5) + geom_point(shape = 21, col = "black")
+
+#PCA with only length width and depth
+tribulusdepth <- clean_d
+
+PrD <- prcomp(tribulusdepth[,8:10], scale. = TRUE)
+PrD
+summary(PrD)
+plot(PrD, type = "l")
+biplot(PrD, scale = 0)
+#Extracting PC scores
+str(PrD)
+PrD$x
+#Adding PC scores to dataset
+View(tribulusdepth)
+depthPC <- cbind(tribulusdepth, PrD$x[,1:2])
+head(depthPC)
+
+#plot with ggplot
+ggplot(depthPC, aes(PC1, PC2, col = island)) + stat_ellipse(geom = "polygon", col = "black", alpha = 0.5) + geom_point(shape = 21, col = "black")
+
+#plots in multiple views
+par(mfrow=c(2,2)
+
 
